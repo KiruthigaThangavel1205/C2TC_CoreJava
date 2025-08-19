56f23ba0d8c75f0123c81850ac5c5a869d789e47
Kiruthiga1205 <velkiruthiga@gmailcom>
2025-08-19 14:29:38 +0530

    Commit...

--- C2TC_CoreJavaConcepts/src/dayfive/multilevelinheritance/vehicle/Car.java
+++ C2TC_CoreJavaConcepts/src/dayfive/multilevelinheritance/vehicle/Car.java
@@ -1,5 +1,13 @@
 package dayfive.multilevelinheritance.vehicle;
 
 public class Car {
+	public Car() {
+		System.out.println("Class Car");
+	}
+
+	public void vehicleType() {
+		System.out.println("Vehicle Type: Car");
+	}
 
 }
+	
