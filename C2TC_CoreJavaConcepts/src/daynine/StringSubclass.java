adab665a6e45707b4aaa16548abcf5b499d93ad2
Kiruthiga1205 <velkiruthiga@gmailcom>
2025-08-25 15:37:17 +0530

    Commit...

--- C2TC_CoreJavaConcepts/src/daynine/StringSubclass.java
+++ C2TC_CoreJavaConcepts/src/daynine/StringSubclass.java
@@ -3,10 +3,10 @@
 //can't inherit final classes
 package daynine;
 
-public class StringSubClass extends String {
+public class StringSubclass {
 
 	public static void main(String[] args) {
 		
-
+		System.out.println("Custom String class - you cannot extend java.lang.String!");
 	}
 }
